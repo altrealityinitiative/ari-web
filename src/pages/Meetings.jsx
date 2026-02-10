fdbd190b13a60c3b3801ae4d09053c1b141dd372
kvho <kvho@umich.edu>
2026-02-10 14:28:17 +0100

    Added google map api and marker

--- src/pages/Meetings.jsx
+++ src/pages/Meetings.jsx
@@ -1,6 +1,17 @@
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
 import "./Meetings.css";
 
+const API_KEY = import.meta.env.VITE_MAP_API_KEY;
+
 function Meetings() {
+  // Duderstadt Center location
+  const duderstadtCenter = { lat: 42.29119670802476, lng: -83.71573319015357 };
+
   return (
     <div className="meetings-page">
       <div className="meetings-header">
@@ -14,13 +25,39 @@ function Meetings() {
           style={{ border: 0 }}
           width="800"
           height="600"
+          title="ARI Calendar"
         ></iframe>
       </div>
+
+      {/* Map Section */}
+      <div className="map-container">
+        <APIProvider
+          apiKey={API_KEY}
+          onLoad={() => console.log("Maps API has loaded.")}
+        >
+          <Map
+            style={{ width: "100%", height: "400px" }}
+            defaultZoom={16}
+            defaultCenter={duderstadtCenter}
+            mapId="DEMO_MAP_ID"
+          >
+            {/* Add Marker */}
+            <AdvancedMarker position={duderstadtCenter}>
+              <Pin
+                background={"#0f9d58"}
+                borderColor={"#006425"}
+                glyphColor={"#60d98f"}
+              />
+            </AdvancedMarker>
+          </Map>
+        </APIProvider>
+      </div>
+
       <section className="meeting-info">
         <p>
           All ARI meetings are on <u>Saturdays</u> 12:00 - 1:30 pm in room 1400
-          (Visualization Lab) at the Duderstadt
-          Center (2281 Bonisteel Blvd, Ann Arbor, MI 48109), unless specified otherwise.
+          (Visualization Lab) at the Duderstadt Center (2281 Bonisteel Blvd, Ann
+          Arbor, MI 48109), unless specified otherwise.
         </p>
       </section>
     </div>
