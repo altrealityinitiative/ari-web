144d6e2c40d29da130b3ef92e087ead9d1f6cade
kvho <kvho@umich.edu>
2026-02-24 19:50:31 +0100

    fixed image asset importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import Home from "./pages/Home";
 import Meetings from "./pages/Meetings";
-// import Workshops from "./pages/Workshops";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/meetings" element={<Meetings />} />
-          {/* <Route path="/workshops" element={<Workshops />} /> */}
-        </Routes>
-        <Footer />
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/meetings" element={<Meetings />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
