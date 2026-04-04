51a5a8e4066468b5fd88ae61acc3f2b673f84c4e
kvho <kvho@umich.edu>
2026-04-04 19:43:26 +0200

    corrected calendar

--- src/pages/Meetings.jsx
+++ src/pages/Meetings.jsx
@@ -4,28 +4,28 @@ import PageTransition from "../components/PageTransition";
 function Meetings() {
   return (
     <PageTransition>
-    <div className="meetings-page">
-      <div className="meetings-header">
-        <h1>Upcoming Meetings</h1>
-        <p>Join us for our regular meetings and events</p>
-      </div>
+      <div className="meetings-page">
+        <div className="meetings-header">
+          <h1>Upcoming Meetings</h1>
+          <p>Join us for our regular meetings and events</p>
+        </div>
 
-      <div className="calendar-container">
-        <iframe
-          src="https://calendar.google.com/calendar/embed?src=7b56973f82c8aeb7e50b6bfcf325bbc9f2e7c07d132f623811e9080c07250c85%40group.calendar.google.com&ctz=Europe%2FPrague"
-          style={{ border: 0 }}
-          width="800"
-          height="600"
-        ></iframe>
+        <div className="calendar-container">
+          <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FDetroit&showPrint=0&showTz=0&src=YWx0cmVhbGl0eWluaXRpYXRpdmVAZ21haWwuY29t&src=N2I1Njk3M2Y4MmM4YWViN2U1MGI2YmZjZjMyNWJiYzlmMmU3YzA3ZDEzMmY2MjM4MTFlOTA4MGMwNzI1MGM4NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19mMGY4OGFhNTMxNjVjNDljZDZhMTFjZDgwOTAzNTU5YWZkN2EyOGU1YTJmNDgxMGIyZTVhNzRmOGFlOTc5N2Y5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5&color=%237986cb&color=%23009688"
+            style= {{ border: 0 }}
+            width="800"
+            height="600"
+          ></iframe>
+        </div>
+        <section className="meeting-info">
+          <p>
+            All ARI meetings are on <u>Saturdays</u> 12:00 - 1:30 pm in room
+            1400 (Visualization Lab) at the Duderstadt Center (2281 Bonisteel
+            Blvd, Ann Arbor, MI 48109), unless specified otherwise.
+          </p>
+        </section>
       </div>
-      <section className="meeting-info">
-        <p>
-          All ARI meetings are on <u>Saturdays</u> 12:00 - 1:30 pm in room 1400
-          (Visualization Lab) at the Duderstadt
-          Center (2281 Bonisteel Blvd, Ann Arbor, MI 48109), unless specified otherwise.
-        </p>
-      </section>
-    </div>
     </PageTransition>
   );
 }
